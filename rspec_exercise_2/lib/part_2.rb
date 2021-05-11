54d999664af5b4896ee43511f0453bd3daa45857
Joe <jkjoeeekim@live.com>
2021-05-11 16:25:51 -0700

    Finish project 1

--- rspec_exercise_2/lib/part_2.rb
+++ rspec_exercise_2/lib/part_2.rb
@@ -1,11 +1,23 @@
-def palindrome?(str)
+require "byebug"
 
+def palindrome?(str)
+    reversed = ""
+    # debugger
+    str.each_char { |char| reversed = char + reversed }
+    reversed == str
 end
 
 def substrings(str)
-
+    substring_array = []
+    (0...str.length).each do |idx1|
+        (idx1...str.length).each do |idx2|
+            substring_array << str[idx1..idx2]
+        end
+    end
+    substring_array
 end
 
 def palindrome_substrings(str)
-    
+    substring_array = substrings(str)
+    substring_array.select { |sub| palindrome?(sub) && sub.length > 1 }
 end
