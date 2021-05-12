3f3523d356e1c622d19f953269e8d820849a5cd6
Joe <jkjoeeekim@live.com>
2021-05-12 09:04:56 -0700

    Finish part 2 project 2

--- nauseating_numbers/nauseating_numbers_part2.rb
+++ nauseating_numbers/nauseating_numbers_part2.rb
@@ -64,11 +64,11 @@ def tribonacci_number(num)
     tribonaccis[num - 1]
 end
 
-p tribonacci_number(1)  == 1
-p tribonacci_number(2)  == 1
-p tribonacci_number(3)  == 2
-p tribonacci_number(4)  == 4
-p tribonacci_number(5)  == 7
-p tribonacci_number(6)  == 13
-p tribonacci_number(7)  == 24
-p tribonacci_number(11) == 274
+# p tribonacci_number(1)  == 1
+# p tribonacci_number(2)  == 1
+# p tribonacci_number(3)  == 2
+# p tribonacci_number(4)  == 4
+# p tribonacci_number(5)  == 7
+# p tribonacci_number(6)  == 13
+# p tribonacci_number(7)  == 24
+# p tribonacci_number(11) == 274
