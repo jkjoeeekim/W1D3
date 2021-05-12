0d7e96c300888c23bfe24a36f4eeffdf156afbe7
Joe <jkjoeeekim@live.com>
2021-05-12 15:34:38 -0700

    Finish mersenne_prime and triangular_word? part 4 project 2

--- nauseating_numbers/nauseating_numbers_part4.rb
+++ nauseating_numbers/nauseating_numbers_part4.rb
@@ -1,30 +1,60 @@
 def mersenne_prime(num)
+    mersenne_primes = [3]
+    until mersenne_primes.length == num
+        i = mersenne_primes.last
+        prime = []
+        (0..(i**i)).any? do |exponent|
+            number = (2 ** exponent) - 1
+            prime << number if is_prime?(number) && !mersenne_primes.include?(number)
+        end
+        mersenne_primes.concat(prime)
+    end
 
+    mersenne_primes.last
 end
 
-# p mersenne_prime(1) # 3
-# p mersenne_prime(2) # 7
-# p mersenne_prime(3) # 31
-# p mersenne_prime(4) # 127
-# p mersenne_prime(6) # 131071
+def is_prime?(num)
+    return false if num < 2
+
+    (2...num).none? { |divisor| num % divisor == 0 }
+end
+
+# p is_prime?(1) == false
+# p is_prime?(2) == true
+# p is_prime?(3) == true
+# p is_prime?(4) == false
+# p is_prime?(6) == false
+# p is_prime?(7) == true
+# p mersenne_prime(1) == 3
+# p mersenne_prime(2) == 7
+# p mersenne_prime(3) == 31
+# p mersenne_prime(4) == 127
+# p mersenne_prime(6) == 131071
+# p mersenne_prime(7) == 524287
+# p mersenne_prime(8) == 2147483648
 
 
 def triangular_word?(str)
-
+    alpha = ('a'..'z').to_a
+    encoded = 0
+    str.each_char { |char| encoded += (alpha.index(char) + 1) }
+    (1..(str.length * 26)).any? do |num|
+        ((num * (num + 1)) / 2) == encoded
+    end
 end
 
-# p triangular_word?('abc')       # true
-# p triangular_word?('ba')        # true
-# p triangular_word?('lovely')    # true
-# p triangular_word?('question')  # true
-# p triangular_word?('aa')        # false
-# p triangular_word?('cd')        # false
-# p triangular_word?('cat')       # false
-# p triangular_word?('sink')      # false
+p triangular_word?('abc')       == true
+p triangular_word?('ba')        == true
+p triangular_word?('lovely')    == true
+p triangular_word?('question')  == true
+p triangular_word?('aa')        == false
+p triangular_word?('cd')        == false
+p triangular_word?('cat')       == false
+p triangular_word?('sink')      == false
 
 
 def consecutive_collapse(array)
-
+    
 end
 
 # p consecutive_collapse([3, 4, 1])                     # [1]
