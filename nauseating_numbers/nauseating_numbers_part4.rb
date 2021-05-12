9f67373b93200d20c7c6d69c2aa3a22f134b7708
Joe <jkjoeeekim@live.com>
2021-05-12 16:35:20 -0700

    Finish part 4 project 2

--- nauseating_numbers/nauseating_numbers_part4.rb
+++ nauseating_numbers/nauseating_numbers_part4.rb
@@ -81,16 +81,34 @@ end
 
 
 def pretentious_primes(array, num)
-
+    array.map! do |ele|
+        # debugger
+        primes = []
+        if num < 0
+            # debugger
+            (0...ele).to_a.reverse.each do |number|
+                # debugger
+                primes << nil if number == 0 && primes.length < num.abs()
+                primes << number if is_prime?(number) && primes.length < num.abs()
+            end
+        else
+            ((ele + 1)..(ele ** 2)).each do |number|
+                # debugger
+                primes << number if is_prime?(number) && primes.length < num.abs()
+            end
+        end
+        # debugger
+        primes.last
+    end
 end
 
-# p pretentious_primes([4, 15, 7], 1)           # [5, 17, 11]
-# p pretentious_primes([4, 15, 7], 2)           # [7, 19, 13]
-# p pretentious_primes([12, 11, 14, 15, 7], 1)  # [13, 13, 17, 17, 11]
-# p pretentious_primes([12, 11, 14, 15, 7], 3)  # [19, 19, 23, 23, 17]
-# p pretentious_primes([4, 15, 7], -1)          # [3, 13, 5]
-# p pretentious_primes([4, 15, 7], -2)          # [2, 11, 3]
-# p pretentious_primes([2, 11, 21], -1)         # [nil, 7, 19]
-# p pretentious_primes([32, 5, 11], -3)         # [23, nil, 3]
-# p pretentious_primes([32, 5, 11], -4)         # [19, nil, 2]
-# p pretentious_primes([32, 5, 11], -5)         # [17, nil, nil]
+p pretentious_primes([4, 15, 7], 1)             == [5, 17, 11]
+p pretentious_primes([4, 15, 7], 2)             == [7, 19, 13]
+p pretentious_primes([12, 11, 14, 15, 7], 1)    == [13, 13, 17, 17, 11]
+p pretentious_primes([12, 11, 14, 15, 7], 3)    == [19, 19, 23, 23, 17]
+p pretentious_primes([4, 15, 7], -1)            == [3, 13, 5]
+p pretentious_primes([4, 15, 7], -2)            == [2, 11, 3]
+p pretentious_primes([2, 11, 21], -1)           == [nil, 7, 19]
+p pretentious_primes([32, 5, 11], -3)           == [23, nil, 3]
+p pretentious_primes([32, 5, 11], -4)           == [19, nil, 2]
+p pretentious_primes([32, 5, 11], -5)           == [17, nil, nil]
